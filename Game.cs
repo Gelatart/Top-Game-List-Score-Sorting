dd15b33c731ef73461d8a3462bf7d414a6437702
Daniel Wood <daniel.f.wood@gmail.com>
2019-05-12 00:12:34 -0600

    Added basic variables to game class, created a starting main class
    At home, 5/12/2019

--- Game.cs
+++ Game.cs
@@ -1,1 +1,17 @@
-
+namespace DefaultNamespace
+{
+    public class Game
+    {
+        public string BaseGame;
+        public string Title;
+        public string CompletionStatus;
+        public int RankedScore;
+        public int InclusionScore;
+        public string Franchise;
+        public string Subfranchise;
+        public string ReleaseDate;
+        //list of Lists on
+        public string SpecialNotes;
+        public bool Discontinued;
+    }
+}
