b4034319a9db8cfea08fa9eadc76522e7ac03263
Daniel <daniel.f.wood@gmail.com>
2019-06-20 01:45:56 -0600

    Added some more basic framework to main function
    At home, 6/20/2019

--- Main.cs
+++ Main.cs
@@ -4,6 +4,7 @@ namespace DefaultNamespace
 {
     public class Main
     {
+        Dictionary<Game, string> GameIndex = new Dictionary<Game, string>();
         static void Main(string[] args)
         {
             // Display the number of command line arguments:
@@ -11,6 +12,14 @@ namespace DefaultNamespace
             foreach (string file in Directory.EnumerateFiles(folderPath, "*.txt")) 
             {
                 string contents = File.ReadAllText(file);
+                string line;
+                while ((line = Console.ReadLine()) != null)
+                {
+                    //Read each variable and put it in an array based on how lines formatted
+                    Game entry;
+                    string Title;
+                    GameIndex.add(entry, Title); //do an if/else check to see if already in dictionary first
+                }
             }
         }
     }
