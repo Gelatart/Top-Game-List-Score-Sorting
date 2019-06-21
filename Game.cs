ded599101d4c302cdaa31f80f02e2049e6436e8e
Daniel <daniel.f.wood@gmail.com>
2019-06-21 02:34:36 -0600

    Added Game constructor function, fleshed out what is grabbed from lines
    At home, 6/21/2019

--- Game.cs
+++ Game.cs
@@ -13,5 +13,19 @@ namespace DefaultNamespace
         //list of Lists on
         public string SpecialNotes;
         public bool Discontinued;
+
+        public Game(string BaseInput, int RankInput, string TitleInput, string CompletionInput, string FranchiseInput, string SubInput, string ReleaseInput, string NotesInput, bool DiscInput)
+        {
+            BaseGame = BaseInput;
+            Title = TitleInput;
+            CompletionStatus = CompletionInput;
+            RankedScore = RankInput;
+            InclusionScore = 1;
+            Franchise = FranchiseInput;
+            Subfranchise = SubInput;
+            ReleaseDate = ReleaseInput;
+            SpecialNotes = NotesInput;
+            Discontinued = DiscInput;
+        }
     }
 }
--- Main.cs
+++ Main.cs
@@ -16,9 +16,18 @@ namespace DefaultNamespace
                 while ((line = Console.ReadLine()) != null)
                 {
                     //Read each variable and put it in an array based on how lines formatted
-                    Game entry;
-                    string Title;
-                    GameIndex.add(entry, Title); //do an if/else check to see if already in dictionary first
+                    string[] attributes = line.Split(';');
+                    string BaseGame = attributes[0];
+                    int Rank = attributes[1];
+                    string Title = attributes[2];
+                    string CompletionStatus = attributes[3];
+                    string Franchise = attributes[4];
+                    string Subfranchise = attributes[5];
+                    string ReleaseDate = attributes[6];
+                    string SpecialNotes = attributes[7];
+                    bool Discontinued = attributes[8];
+                    Game Entry = Game(BaseGame,Rank,Title,CompletionStatus,Franchise,Subfranchise,ReleaseDate,SpecialNotes,Discontinued); //instantiate with new values
+                    GameIndex.add(Entry, BaseGame); //do an if/else check to see if already in dictionary first
                 }
             }
         }
