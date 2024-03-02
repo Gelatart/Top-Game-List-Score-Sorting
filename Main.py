a9c486e377e2a7c4773bec9411d4de2ef9a229b6
Daniel Wood <daniel.f.wood@gmail.com>
2024-03-02 13:22:00 -0700

    Basic ideas for main.py

---
+++ Main.py
@@ -0,0 +1,5 @@
+#INITIAL PLANNING:
+#Goal of this file is to be the main hub
+#Running this could potentially lead to all the other files I have set up, to run different operations
+#So it should probably be pretty command line interface based (Maybe a GUI someday?)
+#
